2a2ce0a8310845f6b1e3dbc31a78122708fd4aed
Subreme <71085002+subreme@users.noreply.github.com>
2021-05-08 23:15:04 +0200

    Added `config_u32()`
    - Added `config_u32()`
    - Removed all comments

--- src/main.rs
+++ src/main.rs
@@ -3,167 +3,76 @@ use std::io;
 use std::process;
 
 fn main() {
-    // This was previously printed with just one `println!`
-    // macro but I divided it to improve legibility
     println!("Password Generator");
     println!("- Press `enter` to use the default value");
-
-    // The `restart` command is currently disabled...
-    /*println!("- Type `restart` to start over");*/
     println!("- Type `exit` to quit\n");
 
-    // This loop has a `'label`, as this allows me to restart the
-    // configuration at any time from within a nested loop
-    /*'main:*/
-    loop {
-        println!("\nHow many characters? (Default: `32`)");
+    let len: u32 = config_u32("How many characters?", 32);
+    let low: bool = config_bool("Include lowercase letters?", true);
+    let upp: bool = config_bool("Include uppercase letters?", true);
+    let num: bool = config_bool("Include numbers?", true);
+    let spe: bool = config_bool("Include special characters?", true);
 
-        // `len` is initially used to store the user's input
-        let mut len = String::new();
+    let mut characters = String::new();
+
+    if low {
+        characters.push_str("abcdefghijklmnopqrstuvwxyz");
+    };
+
+    if upp {
+        characters.push_str("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    };
+
+    if num {
+        characters.push_str("1234567890");
+    };
+
+    if spe {
+        characters.push_str("!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~");
+    };
+
+    println!("\nYour password is: {}", gen(len, &characters));
+
+    loop {
+        println!("\nRun again? (Default: `Yes`)");
+
+        let mut new = String::new();
 
         io::stdin()
-            .read_line(&mut len)
+            .read_line(&mut new)
             .expect("Failed to read input!");
-        // Pattern matching is used to interpret `len` and shadow
-        // it with an integer value (the desired password length)
-        let len: u32 = match len.trim().parse() {
-            // If the trimmed String can be parsed to an integer,
-            // this `if` statement makes sure `len`'s value is
-            // positive, for obvious reasons...
-            Ok(len) => {
-                // As it turns out, the only invalid integer is zero,
-                // since `len`'s `u32` type results in an error if
-                // the parsing of a negative number is attempted
-                if len /*>*/ != 0 {
-                    len
-                } else {
-                    // This error message was edited for the same reason
-                    /*println!("\nPassword length must be a positive integer!");*/
-                    println!("\nPassword length cannot be zero!");
-
-                    // This will repeat the current loop, prompting
-                    // a length value once again
-                    continue;
+        match new.trim().to_lowercase().as_str() {
+            "yes" | "y" | "" => {
+                if new.trim().is_empty() {
+                    println!("Yes");
                 }
+                println!("\nYour password is: {}", gen(len, &characters));
             }
 
-            // Since several input values could result in an error,
-            // several `if` statements are used to react accordingly
-            Err(_) => {
-                // To allow for the autocompletion of the default value,
-                // this checks if the input doesn't contain any characters
-                if len.trim().is_empty() {
-                    // In order to make the program more intuitive, the
-                    // default value is printed when this is the case
-                    println!("32");
-                    32
-                // As mentioned in line 10, "restart" is treated as a
-                // command allowing the user to reset the config values
-                /*} else if len.trim() == "restart" {
-                // By using the outer-most loop's label, the whole
-                // config process can be restarted
-                break 'main;*/
-
-                // "Exit" is also a recognized command, as mentioned in
-                // line 11, allowing users to easily quit the program if
-                // they are not familiat with `^C`
-                } else if len.trim() == "exit" {
-                    // The process is terminated with a default error message
-                    process::exit(1);
-
-                // All other inputs will be considered invalid
-                } else {
-                    println!("Invalid input!");
-                    continue;
-                }
+            "no" | "n" | "exit" => {
+                break;
             }
-        };
-        let low: bool = config_bool("Include lowercase letters?", true);
-        let upp: bool = config_bool("Include uppercase letters?", true);
-        let num: bool = config_bool("Include numbers?", true);
-        let spe: bool = config_bool("Include special characters?", true);
 
-        // Once the configuration is done, a String with all
-        // accepted characters is created as the source for
-        // the password's contents
-        let mut characters = String::new();
-
-        if low {
-            characters.push_str("abcdefghijklmnopqrstuvwxyz");
-        };
-
-        if upp {
-            characters.push_str("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-        };
-
-        if num {
-            characters.push_str("1234567890");
-        };
-
-        if spe {
-            characters.push_str("!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~");
-        };
-
-        // The password is generated and displayed
-        println!("\nYour password is: {}", gen(len, &characters));
-
-        // After the first passoword generation,
-        // the user is allowed to generate more
-        // using the same settings
-        loop {
-            println!("\nRun again? (Default: `Yes`)");
-
-            let mut new = String::new();
-
-            io::stdin()
-                .read_line(&mut new)
-                .expect("Failed to read input!");
-            match new.trim().to_lowercase().as_str() {
-                "yes" | "y" | "" => {
-                    if new.trim().is_empty() {
-                        println!("Yes");
-                    }
-                    println!("\nYour password is: {}", gen(len, &characters));
-                }
-
-                // This input is approached slightly
-                // differently, as ending the loop is
-                // enough to terminate the program
-                "no" | "n" | "exit" => {
-                    break;
-                }
-
-                // As usual, though, the `restart`
-                // command can be used to reset
-                // the password settings
-                /*"restart" => {
-                    break 'main;
-                }*/
-                _ => {
-                    println!("\nPlease input one of the following arguments:");
-                    println!("- `Yes` or `Y`");
-                    println!("- `No` or `N`");
-                    println!("(not case-sensitive)");
-                    continue;
-                }
+            _ => {
+                println!("\nPlease input one of the following arguments:");
+                println!("- `Yes` or `Y`");
+                println!("- `No` or `N`");
+                println!("(not case-sensitive)");
+                continue;
             }
         }
-
-        break;
     }
 }
 
-// This function is the first step in streamlining the
-// configuration process, greatly simplifying the code
 fn config_bool(prompt: &str, default: bool) -> bool {
-    let default: String = if default {
+    let default_text: String = if default {
         String::from("Yes")
     } else {
         String::from("No")
     };
 
     let result: bool = loop {
-        println!("\n{} (Default: `{}`)", prompt, default);
+        println!("\n{} (Default: `{}`)", prompt, default_text);
         let mut x = String::new();
         io::stdin()
             .read_line(&mut x)
@@ -171,10 +80,10 @@ fn config_bool(prompt: &str, default: bool) -> bool {
         match x.trim().to_lowercase().as_str() {
             "yes" | "y" | "" => {
                 if x.trim().is_empty() {
-                    println!("Yes");
+                    println!("{}", default_text);
                 }
 
-                break true;
+                break default;
             }
 
             "no" | "n" => break false,
@@ -189,6 +98,41 @@ fn config_bool(prompt: &str, default: bool) -> bool {
                 println!("- `No` or `N`");
                 println!("(not case-sensitive)");
                 continue;
+            }
+        };
+    };
+
+    result
+}
+
+fn config_u32(prompt: &str, default: u32) -> u32 {
+    let result: u32 = loop {
+        println!("\n{} (Default: `{}`)", prompt, default);
+        let mut x = String::new();
+
+        io::stdin()
+            .read_line(&mut x)
+            .expect("Failed to read input!");
+        match x.trim().parse::<u32>() {
+            Ok(x) => {
+                if x != 0 {
+                    x
+                } else {
+                    println!("\nValue cannot be zero!");
+                    continue;
+                }
+            }
+
+            Err(_) => {
+                if x.trim().is_empty() {
+                    println!("{}", default);
+                    break default;
+                } else if x.trim() == "exit" {
+                    process::exit(1)
+                } else {
+                    println!("Invalid input!");
+                    continue;
+                }
             }
         };
     };
